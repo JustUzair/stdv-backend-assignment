083eb01fa49cc3c4596d7622b4730ba8103ca855
Uzair Saiyed <uzairhajra76330@gmail.com>
2023-08-10 00:46:01 +0530

    Allow only admins to render the pdf

--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const authController = require("../controllers/authController");
 const characterController = require("../controllers/characterController");
-router.get("/pdf-view", characterController.getCharacterPDF);
 router.use(authController.protect, authController.restrictTo("admin"));
+router.get("/pdf-view", characterController.getCharacterPDF);
 router
   .get("/", characterController.getAllCharacters)
   .post("/", characterController.createCharacter);
